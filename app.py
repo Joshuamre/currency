b8c170287ffb3b83de1d73b8dbdc5d415c82303f
Joshuamre <67772843+Joshuamre@users.noreply.github.com>
2023-02-28 17:02:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import requests
+import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 api_key = "7242dd2b7c3816ab6ff463e9"
 
@@ -22,5 +24,15 @@ if from_currency and to_currency:
     converted_amount = currency_converter(amount, from_currency.upper(), to_currency.upper())
     if converted_amount:
         st.write(f"{amount:.1f} {from_currency.upper()} = {converted_amount:.2f} {to_currency.upper()}")
-    else:
-        st.write("Unable to perform currency conversion. Please check your input and try again.")
+        
+        # Get historical exchange rates
+        history_url = f"https://v6.exchangerate-api.com/v6/{api_key}/history/{from_currency}/{to_currency}"
+        history_response = requests.get(history_url)
+        if history_response.status_code == 200:
+            history_data = history_response.json()['history']
+            df = pd.DataFrame(history_data.items(), columns=["Date", "Exchange Rate"])
+            df["Date"] = pd.to_datetime(df["Date"])
+            fig = px.line(df, x="Date", y="Exchange Rate", title=f"{from_currency.upper()}/{to_currency.upper()} Exchange Rates")
+            st.plotly_chart(fig)
+        else:
+            st.write(f"Error: {history_response.json()['error-type']}")
